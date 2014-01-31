461a333b25ccf9be4f3a9b95664625e9a95b1838
Nathan Moeller <ndmoeller@wisc.edu>
2014-01-31 13:39:31 -0600

    fixing heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 DemoApp::Application.routes.draw do
+  root to: "user#index"
   resources :microposts
 
   resources :users
-  root to: "articles#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
